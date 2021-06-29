82fe113bca6f816884b5e566596f63ecff7ec698
spacetu <spacetu@163.com>
2021-06-29 18:57:25 +0800

    feat: init ProjectList

---
+++ src/pages/ProjectList/ProjectSearch.jsx
@@ -0,0 +1,3 @@
+export const ProjectSearch = () => {
+  return <form action=""></form>;
+};
---
+++ src/pages/ProjectList/index.jsx
@@ -0,0 +1,10 @@
+import { ProjectSearch } from "./ProjectSearch";
+import { ProjectTable } from "./ProjectTable";
+export const ProjectList = () => {
+  return (
+    <div>
+      <ProjectSearch />
+      <ProjectTable />
+    </div>
+  );
+};
